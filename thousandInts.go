cfc68918070baa2c487b93dbb3e98a36cbcef6c0
Riley <kmriley@labs.neit.edu>
2019-07-16 22:36:29 -0400

    Changing to use subbenchmarks

--- thousandInts.go
+++ thousandInts.go
@@ -2,6 +2,13 @@ package main
 
 func main() {
 }
-func sum(max int) int {
+func sumFormula(max int) int {
 	return (max * (max + 1) / 2)
+}
+func sumLoop(max int) int {
+	total := 0
+	for val := 0; val < max; val++ {
+		total += val
+	}
+	return total
 }
--- thousandInts_test.go
+++ thousandInts_test.go
@@ -4,18 +4,35 @@ import (
 	"testing"
 )
 
-func BenchmarkThousand(b *testing.B) {
-	for val := 0; val < b.N; val++ {
-		sum(1000)
+func BenchmarkCustomarks(b *testing.B) {
+	benchmarksFormula := []struct {
+		name  string
+		value int
+	}{
+		{"Thousand Formula", 1000},
+		{"Billion Formula", 1000000000},
+		{"Quintillion Formula", 1000000000000000000},
 	}
-}
-func BenchmarkBillion(b *testing.B) {
-	for val := 0; val < b.N; val++ {
-		sum(1000000000)
+	benchmarksLoop := []struct {
+		name  string
+		value int
+	}{
+		{"Thousand Loop", 1000},
+		{"Billion Loop", 1000000000},
+		{"Quintillion Loop", 1000000000000000000},
 	}
-}
-func BenchmarkQuintillion(b *testing.B) {
-	for val := 0; val < b.N; val++ {
-		sum(1000000000000000000)
+	for _, bmF := range benchmarksFormula {
+		b.Run(bmF.name, func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				sumFormula(bmF.value)
+			}
+		})
+	}
+	for _, bmL := range benchmarksLoop {
+		b.Run(bmL.name, func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				sumLoop(bmL.value)
+			}
+		})
 	}
 }
