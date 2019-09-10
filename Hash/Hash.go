71d893c517d269fddc9db1c9204e88b3805000de
Kyle <KMRiley@kari.dev>
2019-09-09 20:38:31 -0400

    adding hashing, init binary

---
+++ Hash/Hash.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func simple(key string) int {
+	fmt.Printf("The key is = %s \n", key)
+	var len = len(key)
+	var sum = 0
+
+	for i := 0; i < len; i++ {
+		var decVal = key[i]
+		sum = sum + int(decVal)
+		fmt.Printf("Character %s = %v \n", string(key[i]), decVal)
+	}
+	return sum
+}
+
+func main() {
+	var index = simple("1007")
+	fmt.Printf("Index number generated from key = %v \n", index)
+}
